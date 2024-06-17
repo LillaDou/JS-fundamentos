a67fe8c4c8c3ef5e411d4dd1836913ed65b79b15
LillaDou <lilla.e.j.du@gmail.com>
2024-06-17 13:37:05 +0200

    Sets y gets

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -3,15 +3,28 @@
 
 class Persona {
 
-    nombre;
-    codigo;
-    frase;
+    nombre = '';
+    codigo = '';
+    frase = '';
+    comida = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase' ) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    //Set: se usa para establecer un valor
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase();//PAra que la comida aparezca en mayúsculas
+    }
+
+    //Get: se usa para recuperar un valor
+    get getComidaFavorita() {
+        return `La comida favorita de ${ this.nombre } es ${ this.comida }`;
+    }
+
+
     //Métodos o funciones dentro de clases
     quienSoy() {
         console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
@@ -26,10 +39,14 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
 const ironman = new Persona('Tony Stark', 'Iron Man', 'Soy Iron Man');
-
-console.log(spiderman);
-console.log(ironman);
+// console.log(ironman);
 
 spiderman.miFrase();
-ironman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman);
+
 
