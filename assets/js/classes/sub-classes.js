6ecfb3b638bc3a4e8fffa51b97b9c1f4e14f274f
LillaDou <lilla.e.j.du@gmail.com>
2024-06-17 14:09:15 +0200

    Extends - Clases con subclases

---
+++ assets/js/classes/sub-classes.js
@@ -0,0 +1,71 @@
+
+class Persona {
+
+    static _conteo = 0;
+    static get conteo() {
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje() {
+        console.log( this.nombre ); //undefined
+        console.log('Hola a todos, soy un método stático');
+
+    }
+
+    nombre = '';
+    codigo = '';
+    frase = '';
+    comida = '';
+
+    constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase' ) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
+
+        Persona._conteo++;
+    }
+
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita de ${ this.nombre } es ${ this.comida }`;
+    }
+
+    quienSoy() {
+        console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+    }
+
+    miFrase() {
+        this.quienSoy();
+        console.log(`${ this.codigo } dice: ${ this.frase }`);
+    }
+
+}
+
+//Extendemos la clase persona dentro de la clase Heroe - todas las propiedades y métodos
+class Heroe extends Persona{
+
+    clan = 'sin clan';
+
+    constructor( nombre, codigo, frase ) {
+        super( nombre, codigo, frase );
+
+        this.clan = 'Los Avengers';
+        //Si queremos usar el 'this.', debemos ponerlo siempre debajo del super
+    }
+
+    quienSoy() {
+        console.log(`Soy ${ this.nombre }, ${ this.clan } `);
+        super.quienSoy();
+        //Añadimos el super. para que este quienSoy no sobreescriba el de la clase Persona,
+        //y para que aparezcan ambos métodos en consola. Es decir, veremos el quienSoy de
+        //Persona y el de Heroe
+    }
+}
+
+const spiderman = new Heroe('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
+// const spiderman = new Heroe();
+console.log(spiderman);
+spiderman.quienSoy();
